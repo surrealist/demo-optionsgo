affc1a660bb06884b341ac9acdc1f683ca4aa482
Suthep Sangvirotjanaphat <suthep@gfbd.co.th>
2022-12-27 10:37:52 +0700

    break1

---
+++ Efin.OptionsGo.Services/AppBase.cs
@@ -0,0 +1,19 @@
+﻿using Efin.OptionsGo.Services.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Efin.OptionsGo.Services
+{
+  public abstract class AppBase
+  {
+    private readonly AppDb db;
+
+    public AppBase(AppDb db)
+    {
+      this.db = db;
+    }
+  }
+}
